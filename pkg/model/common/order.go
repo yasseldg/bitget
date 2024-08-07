669a34b0cbfb7c6fa5b93e72d412da03bbcdf427
Yassel DG <yasseldg@gmail.com>
2024-08-07 08:57:19 +0000

    model common

---
+++ pkg/model/common/order.go
@@ -0,0 +1,31 @@
+package common
+
+type OrderIds struct {
+	OrderId   string `json:"orderId"`
+	ClientOId string `json:"clientOId"`
+}
+
+type Order struct {
+	OrderIds `json:",inline"`
+
+	InstId string `json:"instId"`
+	Symbol string `json:"symbol"`
+
+	Side        string `json:"side"`
+	Price       string `json:"price"`
+	PriceAvg    string `json:"priceAvg"`
+	Size        string `json:"size"`
+	Status      string `json:"status"`
+	OrderType   string `json:"orderType"`
+	OrderSource string `json:"orderSource"`
+
+	PosMode    string `json:"posMode"`
+	PosSide    string `json:"posSide"`
+	TradeSide  string `json:"tradeSide"`
+	MarginCoin string `json:"marginCoin"`
+	MarginMode string `json:"marginMode"`
+	CTime      string `json:"cTime"`
+	UTime      string `json:"uTime"`
+
+	ReduceOnly string `json:"reduceOnly"`
+}
